e6d140a5efe82c66343eba009366f3a88cc70c21
MatthiasKloepperQC <matthias.kloepper@quantco.com>
2023-03-27 00:32:33 +0200

    First commit, empty project and .gitignore

---
+++ Assets/Scenes/BlankScreen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlankScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the background color of the screen to black
+        Camera.main.backgroundColor = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
